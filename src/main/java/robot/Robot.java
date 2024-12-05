0795f3f12371ddaf76b3c674a20c2abbc9b559b2
Yue Hu Zheng <yuehuz0527@gmail.com>
2024-12-05 15:16:28 -0500

    fixed code

--- src/main/java/robot/Robot.java
+++ src/main/java/robot/Robot.java
@@ -19,6 +19,8 @@ import monologue.Logged;
 import monologue.Monologue;
 import org.littletonrobotics.urcl.URCL;
 import robot.Ports.OI;
+import robot.drive.Drive;
+
 
 /**
  * This class is where the bulk of the robot should be declared. Since Command-based is a
@@ -34,7 +36,7 @@ public class Robot extends CommandRobot implements Logged {
   private final PowerDistribution pdh = new PowerDistribution();
 
   // SUBSYSTEMS
-
+  Drive drive = new Drive();
   // COMMANDS
 
   /** The robot contains subsystems, OI devices, and commands. */
@@ -70,7 +72,9 @@ public class Robot extends CommandRobot implements Logged {
   }
 
   /** Configures trigger -> command bindings. */
-  private void configureBindings() {}
+  private void configureBindings() {
+    drive.setDefaultCommand(drive.drive(driver::getLeftY, driver::getRightY));
+  }
 
   /**
    * Command factory to make both controllers rumble.
